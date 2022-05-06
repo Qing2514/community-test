3d1d4d3ea03332d45a0b38fe3c834b3b79d3959b
Larlchester <741001192@qq.com>
2022-05-06 23:26:12 +0800

    test demo

--- src/test/java/com/yx/CommunityApplicationTests.java
+++ src/test/java/com/yx/CommunityApplicationTests.java
@@ -1,13 +1,25 @@
 package com.yx;
 
+import com.yx.controller.BuildingController;
+import com.yx.dao.BuildingMapper;
 import com.yx.model.Building;
 import com.yx.service.impl.BuildingServiceImpl;
 import com.yx.util.SpringUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CommunityApplicationTests {
+
+    @Autowired
+    public BuildingController buildingController;
+
+    @Autowired
+    public BuildingServiceImpl buildingService;
+
+    @Autowired
+    public BuildingMapper buildingMapper;
 
     @Test
     void contextLoads() {
@@ -15,6 +27,24 @@ class CommunityApplicationTests {
         Building byId = service.findById(1L);
         System.out.println("id  = 1 : " + byId);
         System.out.println(service);
+    }
+
+    @Test
+    void controllerTest(){
+        Building building = buildingController.findById(1L);
+        assert building != null && building.getNumbers().equals("16栋");
+    }
+
+    @Test
+    void serviceTest(){
+        Building building = buildingService.findById(1L);
+        assert building != null && building.getNumbers().equals("16栋");
+    }
+
+    @Test
+    void mapperTest(){
+        Building building = buildingMapper.selectById(1L);
+        assert building != null && building.getNumbers().equals("16栋");
     }
 
 }
