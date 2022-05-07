a3f057cbcc42724101d741a8a8a3b8af4cdec310
Qing2514 <2514632453@qq.com>
2022-05-07 15:55:20 +0800

    alter service to public

--- src/test/java/com/yx/CommunityApplicationTests.java
+++ src/test/java/com/yx/CommunityApplicationTests.java
@@ -1,8 +1,5 @@
 package com.yx;
 
-import com.yx.model.Building;
-import com.yx.service.impl.BuildingServiceImpl;
-import com.yx.util.SpringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,10 +8,6 @@ class CommunityApplicationTests {
 
     @Test
     void contextLoads() {
-        BuildingServiceImpl service = SpringUtils.getBean(BuildingServiceImpl.class);
-        Building byId = service.findById(1L);
-        System.out.println("id  = 1 : " + byId);
-        System.out.println(service);
     }
 
 }
